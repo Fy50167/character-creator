b7c8c21014e6411126079918a408097480b4f407
Francis Yang <fy50167@gmail.com>
2024-02-03 19:57:37 -0800

    react router dom setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import './animation.css';
 import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Creator from './pages/Creator';
+import Profile from './pages/Profile';
+import Character from './pages/Character';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/signup',
+                element: <Signup />,
+            },
+            {
+                path: '/create',
+                element: <Creator />,
+            },
+            {
+                path: '/profile/:id',
+                element: <Profile />,
+            },
+            {
+                path: '/character/:id',
+                element: <Character />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <RouterProvider router={router} />
 );
