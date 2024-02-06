78b14736c8b6718017f9ef425ca6c659668a0f34
Francis Yang <fy50167@gmail.com>
2024-02-05 21:19:42 -0800

    updating render camera

--- client/src/pages/Creator.jsx
+++ client/src/pages/Creator.jsx
@@ -20,27 +20,38 @@ export default function Creator() {
             <div className='bg-white w-5/6 h-auto md:h-4/5 rounded-md'>
                 <div className='w-full h-1/2 flex flex-col md:flex-row'>
                     <div className='w-full md:w-1/2 h-full p-4 rounded-md canvas'>
-                        <Canvas shadows>
+                        <Canvas
+                            shadows
+                            camera={{
+                                position: [0, 1, 0],
+                                target: [0, 1, 0],
+                                fov: 30,
+                            }}
+                        >
                             <Experience />
                         </Canvas>
                     </div>
                     <div className='w-full md:w-1/2 flex flex-col justify-evenly items-start p-4'>
-                        <h2 className='text-black font-bold'>Name</h2>
-                        <p className='text-black'>Default Name</p>
-                        <h2 className='text-black font-bold'>Class</h2>
-                        <div className='flex flex-wrap'>
-                            {classes.map((button) => (
-                                <input
-                                    type='button'
-                                    className={`btn ${
-                                        myClass == button ? 'active' : ''
-                                    }`}
-                                    id={button}
-                                    key={button}
-                                    value={button}
-                                    onClick={() => setMyClass(button)}
-                                />
-                            ))}
+                        <div>
+                            <h2 className='text-black font-bold'>Name</h2>
+                            <p className='text-black'>Default Name</p>
+                        </div>
+                        <div>
+                            <h2 className='text-black font-bold'>Class</h2>
+                            <div className='flex flex-wrap'>
+                                {classes.map((button) => (
+                                    <input
+                                        type='button'
+                                        className={`btn ${
+                                            myClass == button ? 'active' : ''
+                                        }`}
+                                        id={button}
+                                        key={button}
+                                        value={button}
+                                        onClick={() => setMyClass(button)}
+                                    />
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
