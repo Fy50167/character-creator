6d33f42fcdc0df2f95f41d09c93907522e0d742e
Francis Yang <fy50167@gmail.com>
2024-02-06 16:38:06 -0800

    maath for easing

--- client/src/pages/Creator.jsx
+++ client/src/pages/Creator.jsx
@@ -18,9 +18,24 @@ export default function Creator() {
 
     return (
         <div className='w-full grow flex justify-center items-center'>
-            <div className='bg-white relative my-8 pb-10 md:m-auto w-5/6 character-div h-auto rounded-md'>
-                <div className='absolute bottom-0 h-10 text-right creator-top center'>
-                    <p className='text-black'>Save</p>
+            <div className='bg-white relative my-8 pb-12 md:m-auto w-5/6 character-div h-auto rounded-md'>
+                <div className='absolute bottom-0 h-12 text-right creator-top center'>
+                    <button>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='h-8 w-8 save-icon'
+                            fill='grey'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                            strokeWidth={2}
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                d='M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4'
+                            />
+                        </svg>
+                    </button>
                 </div>
                 <div className='w-full h-1/2 flex flex-col md:flex-row'>
                     <div className='w-full md:w-1/2 flex flex-col grow p-4 rounded-md canvas'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,32 +1,28 @@
 import CharacterDisplay from '../components/CharacterDisplay';
+import { useState, Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Loader, Environment } from '@react-three/drei';
 
 export default function Home() {
     return (
-        <section className='w-full h-full flex flex-col justify-center items-center'>
+        <section className='w-full grow flex py-8 flex-col justify-center items-center'>
             <div className='text-center'>
                 <h1 className='stylized text-xl md:text-4xl'>
                     Welcome to the Character Creator!
                 </h1>
             </div>
 
-            <div className='flex flex-col justify-center items-center w-4/5 mt-10'>
+            <div className='flex grow flex-col justify-center items-center w-4/5 mt-10'>
                 <h3 className='text-lg md:text-xl'>
                     Here's what people have made...
                 </h3>
-                <div className='bg-white rounded-md w-full mt-4 min-h-80'>
-                    <CharacterDisplay
-                        creator='Created by'
-                        name='Name'
-                        race='Race'
-                        date='Created on'
-                        bold={true}
-                    />
-                    <CharacterDisplay
-                        creator='Created by'
-                        name='Name'
-                        race='Race'
-                        date='Created on'
-                    />
+                <div className='bg-white rounded-md w-full mt-4 grow'>
+                    <Canvas shadows camera={{ position: [0, 0, 100], fov: 15 }}>
+                        <fog attach='fog' args={['#a79', 8.5, 12]} />
+                        <Suspense fallback={null}></Suspense>
+                        <Environment preset='dawn' background blur={0.5} />
+                    </Canvas>
+                    <Loader />
                 </div>
             </div>
         </section>
