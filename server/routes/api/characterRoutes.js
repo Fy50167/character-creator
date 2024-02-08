ac72c0dbf699b937501db32c8490d126ff61a6b5
Francis Yang <fy50167@gmail.com>
2024-02-07 17:08:50 -0800

    routes

---
+++ server/routes/api/characterRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const {
+    getCharacters,
+    getSingleCharacter,
+    createCharacter,
+    updateCharacter,
+    deleteCharacter,
+} = require('../../controllers/characterController');
+
+router.route('/').get(getCharacters).post(createCharacter);
+
+router.route('/:characterId').get(getSingleCharacter).put(updateCharacter).delete(deleteCharacter);
+  
+  
+module.exports = router;
