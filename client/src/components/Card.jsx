21eb5252f30e517c4570b749f58f665eed778587
Francis Yang <fy50167@gmail.com>
2024-02-10 00:14:45 -0800

    stop event propogation

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -30,7 +30,8 @@ export default function Card({ url, ...props }) {
         );
     });
 
-    const goToHere = () => {
+    const goToHere = (event) => {
+        event.stopPropagation();
         navigate(`/categories/${props.className}`);
     };
 
